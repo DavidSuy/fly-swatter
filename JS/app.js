18143b7b10ac91b66ed5b4961c78082b9aaecaa6
David Suy <DavidSuyDev@gmail.com>
2022-08-02 17:36:29 -0700

    connect leaderboad to local storage

--- JS/app.js
+++ JS/app.js
@@ -2,31 +2,37 @@
 
 // Global Variables
 let startGameButton = document.getElementById("startGame");
-let time = 5;
+let time = 3;
+let score = 0;
 let isPlaying = false;
+let userName = "";
 
 // DOM Elements
 let gameContainer = document.getElementById("gameContainer");
 let timer = document.getElementById("countDownTimer");
-let score = document.getElementById("playerScore");
+let scoreDom = document.getElementById("playerScore");
 let difficulty = document.getElementById("difficulty").value;
 let nickname = document.querySelector("input").value;
+let startForm = document.getElementById("startForm");
+let highScoreList = document.querySelector("#leaderBoard ol");
 
 // Event Listener
 startGameButton.addEventListener("click", startGame);
 
 // Event Handler
 function startGame() {
-  hideStartForm();
-  showTimerBar();
-  showFlyzone();
+  hideOrShowElement("startForm", "hide");
+  hideOrShowElement("timer", "show");
+  hideOrShowElement("flyzone", "show");
   startTimer();
   // beginGame();
 }
 
 function endGame() {
-  showLeaderBoard();
-  hideFlyzone();
+  genLeaderBoard();
+  hideOrShowElement("leaderBoard", "show");
+  hideOrShowElement("flyzone", "hide");
+  // hideFlyzone();
 }
 
 // Fly Constructor
@@ -41,24 +47,42 @@ Fly.prototype.genRandLoc = function () {};
 Fly.prototype.renderFly = function () {};
 Fly.prototype.swatted = function () {};
 
+// Factory Helper Functions
+
 // Toggle Elments
-function hideStartForm() {
-  document.getElementById("startForm").style.display = "none";
+function hideOrShowElement(element, hideOrShow) {
+  let el = document.getElementById(element);
+  if (hideOrShow === "hide") {
+    el.style.display = "none";
+  } else if (hideOrShow === "show") {
+    el.style.display = "";
+  }
 }
-function showTimerBar() {
-  document.getElementById("timer").style.display = "";
-}
-function showFlyzone() {
-  document.getElementById("flyzone").style.display = "";
-}
-function hideFlyzone() {
-  document.getElementById("flyzone").style.display = "none";
-}
-function showLeaderBoard() {
-  document.getElementById("leaderBoard").style.display = "";
-}
-function hideLeaderBoard() {
-  document.getElementById("leaderBoard").style.display = "none";
+
+function genLeaderBoard() {
+  // Dummy data
+  // let leaderBoardList = [
+  //   "name: David Score: 1000",
+  //   "name: Migueal Score: 3000",
+  //   "name: Jackson Score: 2000",
+  // ];
+
+  let leaderBoardList = JSON.parse(localStorage.getItem("leaderBoardList"));
+  if (leaderBoardList) {
+    leaderBoardList.push(`PlayerName Score:${score}`);
+  } else {
+    leaderBoardList = [];
+    leaderBoardList.push(`PlayerName Score:${score}`);
+  }
+  localStorage.setItem("leaderBoardList", JSON.stringify(leaderBoardList));
+  let leaderBoardLists = localStorage.getItem("leaderBoardList");
+  console.log(leaderBoardLists);
+
+  for (let list of leaderBoardList) {
+    let li = document.createElement("li");
+    li.textContent = list;
+    highScoreList.appendChild(li);
+  }
 }
 
 function startTimer() {
